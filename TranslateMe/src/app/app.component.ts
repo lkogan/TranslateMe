0a4cac5b5532b41bbc4d1aafd19bec56cf3fdfa7
Leonid Kogan <k_leo@hotmail.com>
2019-05-25 11:42:06 -0500

    First check-in

--- TranslateMe/src/app/app.component.ts
+++ TranslateMe/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TranslateMe';
+  
+  @Input()
+  url: string = "https://lamula.pe";
+  urlSafe: SafeResourceUrl;
+
+  constructor(public sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 }
